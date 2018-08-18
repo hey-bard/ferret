496662f539de978ce2bf4e0e3fc806bdb1554612
Josh Powlison <joshuapowlison@gmail.com>
2018-08-18 07:21:17 -0400

    Add info on query

--- script.js
+++ script.js
@@ -59,4 +59,5 @@ var propertyInfo={
 	,buttons:"An array of all the buttons. Buttons have many attributes: <strong>content</strong> sets the text, <strong>classes</strong> adds listed CSS classes, <strong>css</strong> adds CSS, <strong>tags</strong> are additional terms you can search for the button by, <strong>element</strong> is automatically set to the button's created element and can start unset or <em>null</em>, and <strong>action</strong> is where to go to on clicking the button."
 	,minSize:"The smallest a button can get if it's far back. Defaults to <em>.4</em>."
 	,fps:"How many frames per second to read mouse movements for scrubbing. Does not affect animation smoothness; animations should be smooth regardless of what this is set to. Defaults to <em>10</em>."
+	,query:"Save searches to the querystring, and load searches from the querystring. This allows going back and forward to revisit searches and creating links to specific searches. If <em>null</em>, this will be disabled; if anything else, it will be the query's name in the URL. Defaults to <em>null</em>."
 };
